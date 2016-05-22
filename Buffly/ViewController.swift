89b96c3f9fb0a405234961044af810e29c2bb42f
SEMIH GOKCEOGLU <semih.gokceoglu@agri-t.com>
2016-05-22 03:21:27 -0400

    Initial Commit

---
+++ Buffly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buffly
+//
+//  Created by Semih Gokceoglu on 2016-05-22.
+//  Copyright © 2016 Semih Gokceoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
